837272ae588bdb45365fdb988ba0d7908448939a
anjorlar <adeanjola@gmail.com>
2020-02-22 19:56:58 +0100

    updated route file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,9 @@ const passport = require('passport');
 
 
 module.exports = app => {
-    app.get(
-        '/auth/google',
-        passport.authenticate('google', {
-            scope: ['profile', 'email']
-        })
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
     );
 
     app.get('/auth/google/callback', passport.authenticate('google'))
