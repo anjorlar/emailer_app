3144dd28f2cd86b9bf0ee5b38285cb6ecc668c3f
anjorlar <adeanjola@gmail.com>
2020-02-26 15:59:19 +0100

    added mongoose orm

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 const app = express();
 
 require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+
+// mongodb+srv://anjy:<0luwatomisin>@cluster0-s91o5.mongodb.net/test
