136bebda3be740145c44917e449d3407711454e0
anjorlar <adeanjola@gmail.com>
2020-02-28 17:25:22 +0100

    added logging to connection string

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
-
+console.log('>>>>>>>>>', keys.mongoURI)
 mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
