b0dca9841579a9bf427053adc3219132e51d6cbd
anjorlar <adeanjola@gmail.com>
2020-03-07 20:07:25 +0100

    added redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import './index.css'
+import App from './components/App';
+import reducers from './components/reducers';
 
-ReactDOM.render();
+const store = createStore(() => reducers, {}, applyMiddleware())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root'));
