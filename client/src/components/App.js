45feacfa439bb7729049f506d375267d651a247a
anjorlar <adeanjola@gmail.com>
2020-03-09 09:41:01 +0100

    added materilize css and header component to client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from "./Header";
+const Dashboard = () => <h3>dashboard </h3>
+const SurveyNew = () => <h3>surveyNew</h3>
+const Landing = () => <h1>landing </h1>
+
 
 const App = () => {
     return (
         <div>
-            Hi there
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact={true} path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
