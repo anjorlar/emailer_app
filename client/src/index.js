8a2e6f0ef4589b455cbc66ad5f0adf8385aacf79
anjorlar <adeanjola@gmail.com>
2020-03-10 21:08:31 +0100

    file update

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk';
-import './index.css'
+import './index.css';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(() => reducers, {}, applyMiddleware(reduxThunk))
+const store = createStore(() => reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}>
