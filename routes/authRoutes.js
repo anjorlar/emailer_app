daa66f7d69fcdd6194c0fc8d45c4e4a1d06c1900
anjorlar <adeanjola@gmail.com>
2020-04-04 10:33:03 +0100

    file changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ module.exports = app => {
 
     app.get('/auth/google/callback', passport.authenticate('google'));
     app.get('/api/logout', (req, res) => {
-        console.log('logged out')
+        console.log('logged out');
         req.logout();
         res.send(req.user);
     });
