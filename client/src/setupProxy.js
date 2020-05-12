11c28507744f6d2958928c18819d29aa381c7893
anjorlar <adeanjola@gmail.com>
2020-05-12 15:57:13 +0100

    fixed bug

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = (app) => {
-    console.log(">>>>>>> Setup proxy is ever called");
-    app.use('/auth/google', createProxyMiddleware({
+module.exports = function (app) {
+    app.use(['api', '/auth/google'], createProxyMiddleware({
         target: 'http://localhost:5000',
         changeOrigin: true,
     },
-        "/api/*", {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-    }
     ));
 };
