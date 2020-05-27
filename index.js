625245c314ba1b763031ca49a96398f288b02422
anjorlar <adeanjola@gmail.com>
2020-05-27 18:15:14 +0100

    modified package json

--- index.js
+++ index.js
@@ -39,7 +39,20 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE.ENV === 'production') {
+    // Express will serve up the production assets eg(our main.js or main.css file)
+    app.use(express.static('client/build'));
+
+    //Express will serve up the index.html file if it does not recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
