6ed9f32bb7ba65fa0e499b8bbaa65e181b0c560e
anjorlar <adeanjola@gmail.com>
2020-06-09 10:44:50 +0100

    typo fixes

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE.ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Express will serve up the production assets eg(our main.js or main.css file)
     app.use(express.static('client/build'));
 
     //Express will serve up the index.html file if it does not recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 };
 
