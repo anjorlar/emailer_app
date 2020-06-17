a070213276bd9793abf7f4b279cdcbb20c00a400
anjorlar <adeanjola@gmail.com>
2020-06-17 18:16:44 +0100

    addded mailer.js and emailTemplates

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,10 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY
 };
 
+
+//emailer-app-API-key for send grid
+// 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
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
 
 const Survey = mongoose.model('surveys')
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body
 
         const survey = new Survey({
             title: title,
-            subject: subject,
-            body: body
+            subject,
+            body: body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now(),
         })
+
+        //Where to send a mail
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        try {
+            await mailer.send()
+        } catch (error) {
+            console.error('error sending mailer', error)
+        }
     })
 };
